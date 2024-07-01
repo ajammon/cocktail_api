6a2a7f4c616af5116e2d5783df313bb66743dad5
Aaron Ammon <ajammon1@gmail.com>
2024-06-30 20:56:32 -0600

    fixed logging file

--- src/libraries/logger/LoggerManager.js
+++ src/libraries/logger/LoggerManager.js
@@ -1,26 +1,32 @@
-const winston = require('winston');
+const winston = require("winston");
 
 function createLoggerInstance() {
   const logger = winston.createLogger({
-    level: 'info',
+    level: "info",
     format: winston.format.combine(
       winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
+        format: "YYYY-MM-DD HH:mm:ss",
       }),
       winston.format.errors({ stack: true }),
       winston.format.splat(),
       winston.format.json()
     ),
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: "error.log", level: "error" }),
+      new winston.transports.File({ filename: "combined.log" }),
+    ],
   });
 
   // Add console transport if not in production environment
-  if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env.NODE_ENV, "logging node env");
+  if (process.env.NODE_ENV !== "production") {
     logger.add(
       new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.colorize(),
-          winston.format.simple()
-        ),
+        format: winston.format.simple(),
       })
     );
   }
