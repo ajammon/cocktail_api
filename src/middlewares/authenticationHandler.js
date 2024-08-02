67fe99374ecdaf7f767d130e280d03bba4abad5a
Dominic Pitts <dominicpitts@Dominics-Laptop.lan>
2024-08-02 08:55:59 -0500

    Add api key validation to the authentication.

--- src/middlewares/authenticationHandler.js
+++ src/middlewares/authenticationHandler.js
@@ -1,8 +1,15 @@
 const admin = require("firebase-admin");
 const logger = require("../libraries/logger/LoggerManager");
+
 // Middleware for authenticating access token
 const authenticationToken = async (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
   const accessToken = req.headers.authorization;
+
+  const validApiKeys = require("../../valid_api_keys.json");
+  if (!validApiKeys.keys.contains(apiKey)) {
+    return res.status(401).json({ error: "Unauthorized - Api Key missing or invalid." });
+  }
 
   if (!accessToken || !accessToken.startsWith("Bearer ")) {
     return res
